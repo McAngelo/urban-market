47c48b97e18d1173c3655c8ab5df25758b196aff
Michael Johnson <mcangelo200@gmail.com>
2020-04-25 19:12:10 +0000

    --added the app bar features

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -5,8 +5,28 @@ class HomeScreen extends  StatelessWidget {
 	Widget build(BuildContext context){
 		return new Scaffold(
 	      appBar: new AppBar(
+	      	backgroundColor: Colors.blue[900],
+	      	leading: Icon(Icons.home),
 	        title: new Text('Urban Market'),
-	        automaticallyImplyLeading: false
+	        //automaticallyImplyLeading: false,
+	        actions: <Widget>[
+	        	IconButton(
+	        		icon: Icon(Icons.directions_bike),
+	        		onPressed: (){},
+	        	),
+	        	IconButton(
+	        		icon: Icon(Icons.directions_bus),
+	        		onPressed: (){},
+	        	),
+	        	PopupMenuButton(
+	        		itemBuilder: (BuildContext context){
+	        			return [
+	        				PopupMenuItem(child: Text('Boat')),
+	        				PopupMenuItem(child: Text('Train'))
+	        			];
+	        		}
+	        	)
+	        ],
 	      ),
 	      body: new Center(
 	        child: new Text(
