45c93fc200f647650b6389904d3fdcd4a2be4554
Michael Johnson <mcangelo200@gmail.com>
2020-04-25 20:16:03 +0000

    --minor bug fixes

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,38 +1,51 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends  StatelessWidget {
+	const HomeScreen({Key key}): super(key: key);
+
 	@override
 	Widget build(BuildContext context){
+
+
 		return new Scaffold(
-	      appBar: new AppBar(
-	      	backgroundColor: Colors.blue[900],
-	      	leading: Icon(Icons.home),
-	        title: new Text('Urban Market'),
-	        automaticallyImplyLeading: true,
-	        actions: <Widget>[
-	        	IconButton(
-	        		icon: Icon(Icons.search),
-	        		onPressed: (){},
-	        	),
-	        	/*PopupMenuButton(
-	        		itemBuilder: (BuildContext context){
-	        			return [
-	        				PopupMenuItem(child: Text('Boat')),
-	        				PopupMenuItem(child: Text('Train'))
-	        			];
-	        		}
-	        	)*/
-	        ],
-	      ),
-	      body: new Center(
-	        child: new Text(
-	          'Done!',
-	          style: new TextStyle(
-	            fontWeight: FontWeight.bold,
-	            fontSize: 30.0
-	          ),
-	        ),
-	      ),
+	      	appBar: new AppBar(
+	      		backgroundColor: Colors.blue[900],
+	      		leading: Icon(Icons.home),
+	        	title: new Text('Urban Market'),
+	        	automaticallyImplyLeading: true,
+	        	actions: <Widget>[
+		        	IconButton(
+		        		icon: Icon(Icons.search),
+		        		onPressed: (){},
+		        	),
+	        	],
+	      	),
+	      	body: new Center(
+		        child: new Text(
+		          'Done!',
+		          style: new TextStyle(
+		            fontWeight: FontWeight.bold,
+		            fontSize: 30.0
+		          ),
+		        ),
+	      	),
+	      	drawer: Drawer(
+	      	)
 	    );
 	}
+}
+
+
+class _NewPage extends MaterialPageRoute<Null> {
+	_NewPage(int id) : super(builder: (BuildContext context){
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Page $id'),
+				elevation: 1.0
+			),
+			body: Center(
+				child: Text('Page $id')
+			),
+		);
+	});
 }
