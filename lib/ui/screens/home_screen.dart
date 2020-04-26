802c4c3f03058e45108a4eb672ed1d2fb1a41928
Michael Johnson <mcangelo200@gmail.com>
2020-04-26 09:53:19 +0000

    --added some chnages and a silver app bar

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -147,3 +147,27 @@ class _NewPage extends MaterialPageRoute<Null> {
 		);
 	});
 }
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
--- lib/ui/screens/market_screens/shops.dart
+++ lib/ui/screens/market_screens/shops.dart
@@ -7,10 +7,42 @@ class ShopsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const listTiles = <Widget>[
+      ListTile(
+        title: Text('Tile 0: basic'),
+      ),
+      Divider(),
+      ListTile(
+        leading: Icon(Icons.face),
+        title: Text('Tile 1: with leading/trailing widgets'),
+        trailing: Icon(Icons.tag_faces),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Tile 2 title'),
+        subtitle: Text('subtitle of tile 2'),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Tile 3: 3 lines'),
+        isThreeLine: true,
+        subtitle: Text('subtitle of tile 3'),
+      ),
+      Divider(),
+      ListTile(
+        title: Text('Tile 4: dense'),
+        dense: true,
+      ),
+    ];
+    // Directly returning a list of widgets is not common practice.
+    // People usually use ListView.builder, c.f. "ListView.builder" example.
+    // Here we mainly demostrate ListTile.
+    //return ;
+
     final TextStyle textStyle = Theme.of(context).textTheme.display1;
     return Container(
 	    width: double.infinity,
-        height: double.infinity,
+        //height: double.infinity,
         decoration: BoxDecoration(
         	image: DecorationImage(
             	image: AssetImage('assets/images/home_background.jpg'),
@@ -18,12 +50,11 @@ class ShopsScreen extends StatelessWidget {
           	),
         ),
         child: SafeArea(
-            child: Card(
-	          	color: Colors.white,
-	          	// The elevation determines shade.
-	          	elevation: 10.0,
-	          	child: Center(child: Icon(Icons.store, size: 265.0, color: Colors.cyan)),
-	        ),
+            child: Container(
+                height: 100.0,
+                child: ListView(children: listTiles),
+              ),
+          
         ),
     );
   }
